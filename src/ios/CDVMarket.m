85fb569a95796be6bba373b5991ed5280b0e6087
exolis <48246417+exolis-open-source@users.noreply.github.com>
2025-04-14 11:38:14 +0200

    Update CDVMarket.m pour correspondre au attente d'apple

--- src/ios/CDVMarket.m
+++ src/ios/CDVMarket.m
@@ -2,9 +2,10 @@
 //  CDVMarket.h
 //
 // Created by Miguel Revetria miguel@xmartlabs.com on 2014-03-17.
+// Modified by Brangers on 2025-04-14
 // License Apache 2.0
 
-#include "CDVMarket.h"
+#import "CDVMarket.h"
 
 @implementation CDVMarket
 
@@ -20,12 +21,31 @@
         
         CDVPluginResult *pluginResult;
         if (appId) {
-            NSString *url = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/%@", appId];
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+            // Construction de l'URL de l'App Store
+            NSString *urlString = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/%@", appId];
+            NSURL *url = [NSURL URLWithString:urlString];
+            
+            // Exécution sur le thread principal pour les opérations UI
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if ([[UIApplication sharedApplication] respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+                    // Utilisation de la nouvelle méthode pour iOS 10+
+                    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
+                        if (!success) {
+                            NSLog(@"Erreur lors de l'ouverture de l'URL: %@", url);
+                        }
+                    }];
+                } else {
+                    // Méthode rétrocompatible pour les versions antérieures
+                    BOOL success = [[UIApplication sharedApplication] openURL:url];
+                    if (!success) {
+                        NSLog(@"Erreur lors de l'ouverture de l'URL (méthode dépréciée) : %@", url);
+                    }
+                }
+            });
             
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         } else {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid application id: null was found"];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Identifiant d'application invalide : null trouvé"];
         }
         
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
